8870f7ff5d46b307c58ce46a89810ba8ac12424b
Tim <tim.chae@gmail.com>
2011-01-31 00:04:13 -0500

    finished layout and routes. also added a Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 EdgeucateMe::Application.routes.draw do
   
+  get "users/new"
+
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/home', :to => 'pages#home'
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

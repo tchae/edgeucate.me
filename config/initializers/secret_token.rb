4add8cb752986b10a67e4cce6ac0ab4eb115ed80
Tim <tim.chae@gmail.com>
2011-01-30 22:54:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdgeucateMe::Application.config.secret_token = '7f3de1dd0106de6f6322b53f0aa0cd44e52c078dea21bbe0ebf20fb3a38de687498cf6cd0d54fe38f3d80d898f0ba38829622b37d00190758f2bee899a537a36'

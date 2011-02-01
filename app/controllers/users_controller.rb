2618aa0f143b31d7c388325a897a14678194ff85
Tim <tim.chae@gmail.com>
2011-02-01 07:42:39 -0500

    added user edit/update, index, and destroy actions along with an admin class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,    :only => :destroy
   
   def new
     @title = "Sign up"
@@ -44,6 +45,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_path
+  end
+  
   private
   
     def authenticate
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(edit_user_path(current_user)) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110201092819) do
+ActiveRecord::Schema.define(:version => 20110201115518) do
 
   create_table "links", :force => true do |t|
     t.string   "cat"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20110201092819) do
     t.string   "username"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
